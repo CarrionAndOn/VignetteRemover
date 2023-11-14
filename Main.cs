9c7fd3f3f40ae687afbae441536b23ce6ae3b779
Mae <maya@carrionandon.me>
2023-11-14 12:18:58 -0800

    Rewrite

--- Main.cs
+++ Main.cs
@@ -1,8 +1,10 @@
-﻿using MelonLoader;
+﻿using BoneLib;
+using MelonLoader;
 using UnityEngine;
 using BoneLib.BoneMenu;
 using BoneLib.BoneMenu.Elements;
 using HarmonyLib;
+using VignetteRemover.Melon;
 
 namespace VignetteRemover
 {
@@ -10,16 +12,17 @@ namespace VignetteRemover
     {
         internal const string Name = "Vignette Remover";
         internal const string Description = "Gives you the ability to remove the damage vignette.";
-        internal const string Author = "CarrionAndOn";
+        internal const string Author = "SoulWithMae";
         internal const string Company = "Weather Electric";
         internal const string Version = "1.0.0";
         internal const string DownloadLink = "null";
-        
-        public static bool Enabled = false;
-        private static Remover _remover;
+
+        private static bool _enabled;
+        private static GameObject _vignetteObj;
         public override void OnInitializeMelon()
         {
-            _remover = new Remover();
+            Preferences.Setup();
+            _enabled = Preferences.AutoDisable;
             SetupBonemenu();
         }
         
@@ -28,18 +31,38 @@ namespace VignetteRemover
         {
             public static void Postfix(Player_Health __instance)
             {
-                _remover.AutoDisable();
+                var playerHead = Player.playerHead;
+                _vignetteObj = playerHead.FindChild("Vignetter(Clone)").gameObject;
+                if (_enabled)
+                {
+                    _vignetteObj.SetActive(false);
+                }
             }
         }
-        private void SetupBonemenu()
+        private static void SetupBonemenu()
         {
-            MenuCategory menuCategory = MenuManager.CreateCategory("Vignette Remover", Color.red);
-            menuCategory.CreateBoolElement("Toggle Autodisable", Color.white, Enabled, delegate(bool value)
-            {
-                Enabled = value;
-            });
-            menuCategory.CreateFunctionElement("Enable Vignette", Color.green, _remover.Enable);
-            menuCategory.CreateFunctionElement("Disable Vignette", Color.red, _remover.Disable);
+            MenuCategory mainCat = MenuManager.CreateCategory("Weather Electric", "6FBDFF");
+            MenuCategory menuCategory = mainCat.CreateCategory("Vignette Remover", Color.red);
+            menuCategory.CreateBoolElement("Toggle Autodisable", Color.white, Preferences.AutoDisable.entry.Value, OnBoolChanged);
+            menuCategory.CreateFunctionElement("Enable Vignette", Color.green, Enable);
+            menuCategory.CreateFunctionElement("Disable Vignette", Color.red, Disable);
+        }
+
+        private static void Enable()
+        {
+            _vignetteObj.SetActive(true);
+        }
+
+        private static void Disable()
+        {
+            _vignetteObj.SetActive(false);
+        }
+        
+        private static void OnBoolChanged(bool value)
+        {
+            _enabled = value;
+            Preferences.AutoDisable.entry.Value = value;
+            Preferences.Category.SaveToFile(false);
         }
     }
 }
---
+++ Melon/Preferences.cs
@@ -0,0 +1,18 @@
+﻿using BoneLib;
+using MelonLoader;
+
+namespace VignetteRemover.Melon
+{
+    internal static class Preferences
+    {
+        public static readonly MelonPreferences_Category Category = MelonPreferences.CreateCategory("Paranoia");
+
+        public static ModPref<bool> AutoDisable;
+
+        public static void Setup()
+        {
+            AutoDisable = new ModPref<bool>(Category, "AutoDisable", false, "Auto Disable Vignette", "Whether to auto disable the vignette object or not.");
+            Category.SaveToFile(false);
+        }
+    }
+}
